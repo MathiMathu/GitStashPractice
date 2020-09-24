a2bce32126fa6c52f57d917cfad52cb013898778
[justillasoft] <[justillasoft21@gmail.com]>
2020-09-25 05:04:53 +0530

    Modified with methods

---
+++ Stash.java
@@ -0,0 +1,44 @@
+public String getStashId()
+ {
+	return StashId;
+ }
+ 
+ public String getStashName()
+ {
+    return StashName;
+ }
+ 
+ public String getStashMessage()
+ {
+    return StashMessage;
+ }
+ 
+ public void setStashId(String SId)
+ {
+	StashId = SId;
+ }
+ 
+ public void setStashName(String SName)
+ {
+	StashName = SName;
+ }
+ 
+ public void setStashMessage(String SMessage)
+ {
+	StashMessage = SMessage;
+ }
+ 
+ public void addStash(Stash s1,Stash s2)
+ {
+  
+ }
+ 
+ public void removeStash(Stash s1)
+ {
+  
+ }
+ 
+ public void clearAllStash()
+ {
+  
+ }
